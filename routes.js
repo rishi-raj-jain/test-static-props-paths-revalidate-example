911715c4038770ac45606b1ca21b1d164fad1f94
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-24 16:55:46 +0530

    Update router

--- routes.js
+++ routes.js
@@ -1,8 +1,21 @@
-const { Router } = require('@layer0/core/router')
-const { nextRoutes } = require('@layer0/next')
+const { Router } = require('@layer0/core/router');
+const { nextRoutes } = require('@layer0/next');
 
 module.exports = new Router()
   .match('/service-worker.js', ({ serviceWorker }) => {
-    return serviceWorker('.next/static/service-worker.js')
+    return serviceWorker('.next/static/service-worker.js');
   })
-  .use(nextRoutes)
+  .match('/tire-check/:path', ({ cache, removeUpstreamResponseHeader }) => {
+    removeUpstreamResponseHeader('set-cookie');
+    cache({
+      browser: {
+        maxAgeSeconds: 0,
+      },
+      edge: {
+        forcePrivateCaching: true,
+        maxAgeSeconds: 5,
+        staleWhileRevalidateSeconds: 5,
+      },
+    });
+  })
+  .use(nextRoutes);
